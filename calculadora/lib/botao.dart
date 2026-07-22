aa89a0a925d443581581397c2ffd9cf0ad4790e6
Guilherme Zaguini <guilherme.zaguini2018@gmail.com>
2026-07-22 18:16:57 -0300

    Adicionando Cores nos Botoes

--- calculadora/lib/botao.dart
+++ calculadora/lib/botao.dart
@@ -12,19 +12,33 @@ class Botao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color cor = Colors.grey;
+    if(texto == '+'|| texto == '-' || texto == '*' || texto == '/'){
+          cor = Colors.orange;
+        }else if(texto == 'C'){
+          cor = Colors.red;
+        }else if(texto == '='){
+          cor = Colors.green;
+        }else if(texto == '⌫'){
+          cor = Colors.yellow;
+        }else{
+          cor = Colors.grey;
+        }
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.all(4),
         child: ElevatedButton(
           onPressed: onPressed,
           style: ElevatedButton.styleFrom(
+            backgroundColor: cor,
             minimumSize: const Size(0, 70),
           ),
           child: Text(
             texto,
-            style: const TextStyle(fontSize: 24),
+            style: const TextStyle(fontSize: 24 ,color: Colors.white),
           ),
         ),
+        
       ),
     );
   }
